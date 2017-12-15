cdf77bdd2cfe7286ace2e6006af8f4f6a94ad8fe
marakov <creator100@mail.ru>
2017-12-15 20:08:05 +0300

    commit

--- src/main/java/com/marakogr/boot/DemoApplication.java
+++ src/main/java/com/marakogr/boot/DemoApplication.java
@@ -2,18 +2,29 @@ package com.marakogr.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer{
+
+//    public static void main(String[] args) {
+////		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+////		System.out.println(( (RestClient) context.getBean("restClient")).hello());
+//        SpringApplication.run(DemoApplication.class, args);
+//
+//    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
-//		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-//		System.out.println(( (RestClient) context.getBean("restClient")).hello());
         SpringApplication.run(DemoApplication.class, args);
-
     }
 
     @Bean
